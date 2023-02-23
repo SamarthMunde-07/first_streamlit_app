5810daf212a8a395d052f11996dd13a8f122007c
SamarthMunde-07 <125958556+SamarthMunde-07@users.noreply.github.com>
2023-02-23 14:52:34 +0530

    Update streamlit_app.py
    added try and expect condition to gets things working

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 
 import streamlit
 import pandas
+import requests
 import snowflake.connector
 from urllib.error import URLError
 
@@ -27,18 +28,17 @@ streamlit.dataframe(fruits_to_show)
 
 #TO GET FUTIVICE API RESPONSE I.E BY REQUESTS
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-
-# normalizes the json format to normal form
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# displays the data on frame in table format
-streamlit.dataframe(fruityvice_normalized)
-
-
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+   else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+expect URLError as e:
+  streamlit.error()
+  
 #below this will not run
 streamlit.stop()
 
