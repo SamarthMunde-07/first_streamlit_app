21dc2c16c9349af850a1d850accf12d533ac564e
SamarthMunde-07 <125958556+SamarthMunde-07@users.noreply.github.com>
2023-02-23 11:49:21 +0530

    Update streamlit_app.py
    Added a request api

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,7 @@ streamlit.dataframe(fruits_to_show)
 
 
 #TO GET FUTIVICE API RESPONSE I.E BY REQUESTS
+streamlit.header('Fruityvice Fruit Advice!')
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
